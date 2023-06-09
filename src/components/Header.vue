e6bd70f03bc4539660899fdc6cd10a4991aace7e
DannielNavas <dannielnavas@gmail.com>
2023-06-09 08:32:43 -0500

    ✨ Home, layout and Header

---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <header>
+    <img
+      src="@/assets/cashflow-icon_070076db-e83a-4afa-b18d-ee058c2b646e.svg"
+      alt="Logo"
+      @click="reload"
+    />
+    <h1><span>Cahs</span>Flow</h1>
+    <span></span>
+  </header>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Header",
+  methods: {
+    reload() {
+      window.location.reload();
+    },
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100vw;
+}
+img,
+span {
+  width: 48px;
+}
+h1 {
+  font-size: 1.4rem;
+  color: var(--brand-blue);
+}
+h1 span {
+  color: var(--brand-green);
+}
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,4 +1,22 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <div>Hola mundo</div>
+  <Layaout>
+    <template #header>
+      <Header></Header>
+    </template>
+  </Layaout>
 </template>
+
+<script>
+import Layaout from "@/components/Layout.vue";
+import Header from "@/components/Header.vue";
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Home",
+  components: {
+    Layaout,
+    Header,
+  },
+};
+</script>
